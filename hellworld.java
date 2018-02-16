2f152fb0952a835813474c7a5d3f275e8a95912c
Mark <mark.kurtz@quest.com>
2018-02-16 10:49:40 -0800

    hello world 1

---
+++ hellworld.java
@@ -0,0 +1,9 @@
+/** 
+This is a minimum Java application
+@version 1.0
+*/
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.println("Hello, World!");
+   }
+}
